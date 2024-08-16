bea829701039c8782e20215a5d5b62499173a38c
brunosm16 <brunoskr23@gmail.com>
2024-08-16 16:23:27 -0300

    feat: 🚀 add comparative-clauses

---
+++ packages/core/src/interfaces/comparative-clauses.ts
@@ -0,0 +1,36 @@
+export interface ComparativeBooleanClauses {
+  is?: boolean | null;
+  isNot?: boolean | null;
+}
+
+export interface ComparativeRangeClauses<ClauseType>
+  extends ComparativeBooleanClauses {
+  eq?: ClauseType;
+  gt?: ClauseType;
+  gte?: ClauseType;
+  in?: ClauseType[];
+  lt?: ClauseType;
+  lte?: ClauseType;
+  neq?: ClauseType;
+  notIn?: ClauseType[];
+}
+
+export interface ComparativeLikeClauses
+  extends ComparativeRangeClauses<string> {
+  iLike?: string;
+  like?: string;
+  notILike?: string;
+  notLike?: string;
+}
+
+export type ComparativeClauses<ClauseType> = ClauseType extends
+  | boolean
+  | never
+  | undefined
+  ? ComparativeBooleanClauses
+  : ClauseType extends string
+    ? ComparativeLikeClauses
+    : ComparativeRangeClauses<ClauseType>;
+
+export type ComparativeClausesKeys<ClauseType> =
+  keyof ComparativeClauses<ClauseType>;
