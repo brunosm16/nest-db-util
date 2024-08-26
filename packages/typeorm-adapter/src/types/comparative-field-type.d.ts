866cccd41629cc42e7dbcd5f7a4b923b9711394a
brunosm16 <brunoskr23@gmail.com>
2024-08-25 22:26:39 -0300

    refactor: 📝 use more declarative parameter name

--- packages/typeorm-adapter/src/types/comparative-field-type.d.ts
+++ packages/typeorm-adapter/src/types/comparative-field-type.d.ts
@@ -1,9 +1,9 @@
 type ComparativeBooleanValues = false | true;
-type ComparativeEntityKeysType<Entity, K extends keyof Entity> =
-  | Entity[K]
-  | Entity[K][];
+type ComparativeEntityKeysType<Entity, Field extends keyof Entity> =
+  | Entity[Field]
+  | Entity[Field][];
 
-export type ComparativeFieldType<Entity, K extends keyof Entity> =
+export type ComparativeFieldType<Entity, Field extends keyof Entity> =
   | ComparativeBooleanValues
-  | ComparativeEntityKeysType<Entity, K>
+  | ComparativeEntityKeysType<Entity, Field>
   | null;
