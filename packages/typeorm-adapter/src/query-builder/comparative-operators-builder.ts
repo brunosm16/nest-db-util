9d10ffef1e637bab2093a6dc23396a45e4f32025
brunosm16 <brunoskr23@gmail.com>
2024-08-25 22:22:11 -0300

    feat: 🚀 count number of parameters

--- packages/typeorm-adapter/src/query-builder/comparative-operators-builder.ts
+++ packages/typeorm-adapter/src/query-builder/comparative-operators-builder.ts
@@ -3,7 +3,18 @@ import type { Repository } from 'typeorm';
 import { QueryBuilderAbstract } from './query-builder.abstract';
 
 export class ComparativeOperatorsBuilder<T> extends QueryBuilderAbstract<T> {
+  private numberOfParameters: number = 0;
+
   constructor(public readonly repository: Repository<T>) {
     super(repository);
+  }
+
+  private getLastParameter(): string {
+    return `params${this.numberOfParameters}`;
+  }
+
+  private incrementNumberOfParameters(): number {
+    this.numberOfParameters += 1;
+    return this.numberOfParameters;
   }
 }
